3675b84905d79a19cf324c815c067c7229574cd2
Unknown <randomsimpson@gmail.com>
2018-03-07 23:49:24 -0600

    Project Beginnings
    Unity project created and placed in repo folder.
    Saved "TestScene".
    Created LevelManager gameobject and added "RoomBuilder" script.
    Created placeholder prefabs for Player and Flooring.
    RoomBuilder script sets room bounds and spawns flooring within. It also spawns the player, along with the game's main camera as a child of the player.
    Added "PlayerMove" script to player prefab and added 2D movement functionality (forward/back, left/right).

---
+++ Infil-Trainer 2018/Assets/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	Transform camObject;
+
+	float moveSpeed;
+
+
+	void Awake () {
+	}
+
+
+	void Start () {
+		print (transform.childCount);
+		camObject = transform.GetChild (0);
+	}
+	
+
+	void Update () {
+		Move ();
+		Rotate ();
+	}
+
+
+	void Move () {
+		moveSpeed = 1.0f * Time.deltaTime;
+
+		if (Input.GetAxis("Horizontal") != 0.0) {
+			transform.position = transform.position + Vector3.right * moveSpeed * Input.GetAxis("Horizontal");
+		}
+		if (Input.GetAxis("Vertical") != 0.0f) {
+			transform.position = transform.position + Vector3.forward * moveSpeed * Input.GetAxis("Vertical");
+		}
+	}
+
+
+	void Rotate () {
+		
+	}
+}
---
+++ Infil-Trainer 2018/Assets/_Scripts/RoomBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBuilder : MonoBehaviour {
+	[SerializeField] GameObject player;
+	[SerializeField] GameObject floorPlane;
+
+	[SerializeField] int roomWidth;
+	[SerializeField] int roomDepth;
+
+
+	void Awake () {
+		roomWidth = Random.Range (4, 10);
+		roomDepth = Random.Range (4, 10);
+
+		LayFloor ();
+	}
+
+
+	void Start () {
+		SpawnPlayer ();
+	}
+	
+
+	void Update () {
+		
+	}
+
+
+	void LayFloor () {
+		Vector3 floorScale = new Vector3 (0.1f, 0.1f, 0.1f);
+		GameObject floorParent = new GameObject ();
+		floorParent.name = "FloorParent";
+		floorParent.transform.parent = gameObject.transform;
+
+		for (int rD = 0; rD < roomDepth; rD++) {
+			for (int rW = 0; rW < roomWidth; rW++) {
+				Vector3 floorPos = new Vector3 (rW, 0.0f, rD);
+
+				floorPlane = Instantiate (floorPlane, floorPos, Quaternion.identity, floorParent.transform);
+				floorPlane.transform.localScale = floorScale;
+			}
+		}
+	}
+
+
+	void SpawnPlayer () {
+		Vector3 playerPos = new Vector3 (roomWidth / 2, 1.0f, 1.0f);
+		GameObject Player = Instantiate (player, playerPos, Quaternion.identity);
+
+		//Setup Main Camera
+		GameObject camEmpty = new GameObject ();
+		camEmpty.name = "CamEmpty";
+		camEmpty.transform.position = Player.transform.position;
+		camEmpty.transform.parent = Player.transform;
+		camEmpty.AddComponent<Camera> ();
+	}
+}
