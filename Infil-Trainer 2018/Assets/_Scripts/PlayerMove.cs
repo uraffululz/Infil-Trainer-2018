58fc3e8181eeeafd9126adc7bd7fb333222f01e9
Unknown <randomsimpson@gmail.com>
2018-03-09 07:34:41 -0600

    Wall-Crawling
    Player now uses raycasting to interact with the room's various surfaces, allowing them to change the incline of their movement (by either choosing to walk/crawl on the floor, or crawl on the walls or ceiling). The player then rotates to "stand" on that surface.
    If trying to move from a "crawling" to a "standing" stance while on the walls or ceiling, the player will fall to a standing position on the floor.

--- Infil-Trainer 2018/Assets/_Scripts/PlayerMove.cs
+++ Infil-Trainer 2018/Assets/_Scripts/PlayerMove.cs
@@ -9,6 +9,7 @@ public class PlayerMove : MonoBehaviour {
 
 	enum Stances {standing, crawling};
 	Stances myStance;
+	string currentSurface;
 
 
 	void Awake () {
@@ -30,10 +31,12 @@ public class PlayerMove : MonoBehaviour {
 			Move (1.0f * Time.deltaTime);
 			Rotate ();
 			ChangeStance ();
+			ChangeIncline ();
 		} else if (myStance == Stances.crawling) {
 			Move (0.5f * Time.deltaTime);
 			Rotate ();
 			ChangeStance ();
+			ChangeIncline ();
 		}
 	}
 
@@ -50,46 +53,82 @@ public class PlayerMove : MonoBehaviour {
 
 
 	void Rotate () {
-//TODO Clamp mouse pointer to screen bounds, maybe using Input.mouseposition
-		if (Input.mousePosition.x <= camObject.GetComponent<Camera>().pixelWidth * 0.4f) {
-			float rotSpeed = camObject.GetComponent<Camera>().pixelWidth/Input.mousePosition.x;
-			transform.Rotate (-Vector3.up * rotSpeed * Time.deltaTime);
-			print (rotSpeed);
+		if (Input.mousePosition != null &&
+			Input.mousePosition.x > 0.0f && Input.mousePosition.x < (float)camObject.GetComponent<Camera> ().pixelWidth
+		    && Input.mousePosition.y > 0.0f && Input.mousePosition.y < (float)camObject.GetComponent<Camera> ().pixelHeight) {
 
-		} else if (Input.mousePosition.x >= camObject.GetComponent<Camera>().pixelWidth * 0.6f) {
-			float rotSpeed = camObject.GetComponent<Camera>().pixelWidth/(800 - Input.mousePosition.x);
-			transform.Rotate (Vector3.up * rotSpeed * Time.deltaTime);
-			print (rotSpeed);
+			if (Input.mousePosition.x <= camObject.GetComponent<Camera> ().pixelWidth * 0.4f) {
+				float rotSpeed = camObject.GetComponent<Camera> ().pixelWidth / Input.mousePosition.x;
+				transform.Rotate (-Vector3.up * rotSpeed * Time.deltaTime);
+			} else if (Input.mousePosition.x >= camObject.GetComponent<Camera> ().pixelWidth * 0.6f) {
+				float rotSpeed = camObject.GetComponent<Camera> ().pixelWidth / (800 - Input.mousePosition.x);
+				transform.Rotate (Vector3.up * rotSpeed * Time.deltaTime);
+			}
 
-		}
-
-		if (Input.mousePosition.y <= camObject.GetComponent<Camera>().pixelHeight * 0.4f) {
-			float rotSpeed = camObject.GetComponent<Camera>().pixelHeight/Input.mousePosition.y;
-			camObject.transform.Rotate (Vector3.right * rotSpeed * Time.deltaTime);
-			print (rotSpeed);
-
-		} else if (Input.mousePosition.y >= camObject.GetComponent<Camera>().pixelHeight * 0.6f) {
-			float rotSpeed = camObject.GetComponent<Camera>().pixelHeight/(600 - Input.mousePosition.y);
-			camObject.transform.Rotate (-Vector3.right * rotSpeed * Time.deltaTime);
-			print (rotSpeed);
-
+			if (Input.mousePosition.y <= camObject.GetComponent<Camera> ().pixelHeight * 0.4f) {
+				float rotSpeed = camObject.GetComponent<Camera> ().pixelHeight / Input.mousePosition.y;
+				camObject.transform.Rotate (Vector3.right * rotSpeed * Time.deltaTime);
+			} else if (Input.mousePosition.y >= camObject.GetComponent<Camera> ().pixelHeight * 0.6f) {
+				float rotSpeed = camObject.GetComponent<Camera> ().pixelHeight / (600 - Input.mousePosition.y);
+				camObject.transform.Rotate (-Vector3.right * rotSpeed * Time.deltaTime);
+			}
 		}
 	}
 
 	void ChangeStance () {
 		if (myStance == Stances.standing) {
+			Vector3 playerScale = Vector3.one * 0.5f;
+			transform.localScale = playerScale;
+
 			if (Input.GetKeyDown(KeyCode.LeftControl)) {
 				myStance = Stances.crawling;
-				Vector3 playerScale = new Vector3 (0.5f, 0.25f, 0.5f);
-				transform.localScale = playerScale;
 			}
 		} else if (myStance == Stances.crawling) {
+			Vector3 playerScale = new Vector3 (0.5f, 0.25f, 0.5f);
+			transform.localScale = playerScale;
+
 			if (Input.GetKeyDown(KeyCode.LeftControl)) {
 				myStance = Stances.standing;
-				Vector3 playerScale = Vector3.one * 0.5f;
-				transform.localScale = playerScale;
+
+				if (currentSurface == "Wall" || currentSurface == "Ceiling") {
+					FallToFloor ();
+				}
 			}
 		}
-		print (myStance);
+	}
+
+
+	void ChangeIncline () {
+		Ray reachForward = new Ray (transform.position, transform.forward);
+		RaycastHit reachedForward;
+		Debug.DrawRay (transform.position, transform.forward, Color.red);
+
+		if (Physics.Raycast(reachForward, out reachedForward, 1.0f)) {
+			if (reachedForward.collider.CompareTag ("Floor") ||
+				reachedForward.collider.CompareTag ("Wall") ||
+				reachedForward.collider.CompareTag ("Ceiling")) {
+				print ("Press E key to change incline");
+
+				if (Input.GetKeyDown(KeyCode.E)) {
+					StickToSurface (-reachedForward.normal * 9.8f, reachedForward);
+					myStance = Stances.crawling;
+					currentSurface = reachedForward.collider.tag;
+					print ("Current Surface: " + currentSurface);
+				}
+			}
+		}
+	}
+
+
+	void StickToSurface (Vector3 gravDir, RaycastHit reachedSurface) {
+		Physics.gravity = gravDir;
+		transform.rotation = Quaternion.FromToRotation (transform.up, reachedSurface.normal) * rb.rotation;
+	}
+
+
+	void FallToFloor () {
+		//Change direction of gravity to WORLD DOWN and rotate player so he lands feet-down
+		Physics.gravity = Vector3.down * 9.8f;
+		transform.rotation = Quaternion.FromToRotation (transform.up, Vector3.up) * rb.rotation;
 	}
 }
